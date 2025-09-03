8c4b3723d1606b46e226ef2a9f7fdc1feda99727
DAjayi <dr716500@dal.ca>
2025-09-03 15:25:46 -0300

    feat: add Button component with Storybook integration and Bootstrap styling

---
+++ src/frontend/src/components/CourseCard.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import  'bootstrap/dist/css/bootstrap.min.css';
+
+</script>
+
+<template>
+  <BCard
+    title="Card Title"
+    img-src="https://picsum.photos/id/25/600/300"
+    img-alt="Image"
+    img-top
+    tag="article"
+    style="max-width: 20rem"
+  >
+  </BCard>
+
+</template>
+
+<style scoped>
+
+</style>
