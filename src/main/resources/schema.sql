a14eec50d0b7cc04cf93a2e261bb3cd63ae9d737
DAjayi <dr716500@dal.ca>
2025-09-17 11:57:17 -0300

    fixing db config issues

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -8,9 +8,11 @@ CREATE TABLE IF NOT EXISTS course (
   priority TEXT,
   term     TEXT,                      -- enum name via converter
   year     INTEGER NOT NULL,          -- store as int
+  status TEXT,
 
   CHECK (priority IS NULL OR priority IN ('high','medium','low')),
   CHECK (term IS NULL OR term IN ('fall','winter','spring','summer')),
+  CHECK ( status IS NOT NULL or status IN ('active' , 'closed' , 'upcoming')),
   CHECK (year BETWEEN 1900 AND 3000)
 );
 
@@ -20,12 +22,12 @@ CREATE TABLE IF NOT EXISTS task (
     name       TEXT NOT NULL,
     due        TEXT,                        -- ISO 'yyyy-MM-dd''T''HH:mm'
     priority   TEXT,
-    taskStatus     TEXT,
+    status     TEXT,
     course_id  INTEGER,
 
     -- Enforce enum-like values written by your converters
     CONSTRAINT priority_constraint CHECK (priority IS NULL OR priority IN ('high','medium','low')),
-    CONSTRAINT status_constraint CHECK (taskStatus   IS NULL OR taskStatus   IN ('not_started','in_progress','completed')),
+    CONSTRAINT status_constraint CHECK (status   IS NULL OR status   IN ('not_started','in_progress','completed')),
 
     FOREIGN KEY (course_id)
     REFERENCES course(id)
